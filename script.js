69cc70e515d21f02dcbd155cf29e200a8337fe4c
MAHIR <157117618+mahircce@users.noreply.github.com>
2024-11-07 10:45:56 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let number =prompt("Enter a number");
+
+for(let x=1;x<=10;x++){
+       document.write(`${number} x ${x} = ${number * x}  <br>`);
+       console.log(`${number} x ${x} = ${number * x}`);
+}
+/*T H MAHIR*/
